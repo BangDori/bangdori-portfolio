a55d7150e5b2c17ed772dcec0acda123d06ef807
BangDori <qudwns8865@naver.com>
2025-06-05 23:56:00 +0900

    어바웃 섹션 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from './_components/AboutSection';
 import ProfileCard from './_components/ProfileCard';
 
 export default function Home() {
   return (
     <main className="mx-auto w-full max-w-6xl px-8 py-16">
       <ProfileCard />
+      <AboutSection />
     </main>
   );
 }
