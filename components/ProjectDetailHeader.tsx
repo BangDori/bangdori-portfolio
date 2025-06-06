0cea916606f8b1b30ab0ca701f99f04f529f4f59
BangDori <qudwns8865@naver.com>
2025-06-06 19:46:49 +0900

    뒤로가기 크기 수정

--- components/ProjectDetailHeader.tsx
+++ components/ProjectDetailHeader.tsx
@@ -12,9 +12,9 @@ export default function ProjectDetailHeader({ title, Icon, date }: ProjectDetail
     <>
       <Link
         href="/"
-        className="group flex w-fit items-center gap-2 border-b border-transparent text-base text-gray-400 hover:text-gray-700"
+        className="group flex w-fit items-center gap-1 border-b border-transparent text-sm text-gray-500 hover:text-gray-700"
       >
-        <ArrowLeft size={16} />
+        <ArrowLeft size={14} />
         <span>뒤로가기</span>
       </Link>
       <div className="flex items-center gap-2">
