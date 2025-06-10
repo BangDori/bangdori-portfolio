8251b34ee1753c3cdc60a7d264d5976c9218b948
BangDori <qudwns8865@naver.com>
2025-06-10 23:54:14 +0900

    다크 모드 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from '@/components/theme/ThemeToggle';
 import AboutSection from './_components/AboutSection';
 import ProfileCard from './_components/ProfileCard';
 import ProjectsSummarySection from './_components/ProjectsSummarySection';
@@ -5,7 +6,10 @@ import TechStackSection from './_components/TechStackSection';
 
 export default function Home() {
   return (
-    <main className="mx-auto w-full max-w-6xl px-8 pt-8 pb-16 md:pt-16">
+    <main className="relative mx-auto w-full max-w-6xl px-8 pt-8 pb-16 md:pt-16">
+      <div className="relative" style={{ paddingTop: 0, paddingRight: 0 }}>
+        <ThemeToggle />
+      </div>
       <ProfileCard />
       <AboutSection />
       <TechStackSection />
